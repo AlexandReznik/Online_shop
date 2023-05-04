1433437fb01c50302fe291b7762e4da0f6a2c283
AlexandrReznik <saha.reznik2004@gmail.com>
2023-05-04 13:31:41 +0300

    ..../\....

--- config/settings.py
+++ config/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = str(os.getenv('SECRET_KEY'))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -176,7 +176,6 @@ LOGGING = {
 }
 
 
-ALLOWED_HOSTS = ["*"]
 if DEBUG:
     INTERNAL_IPS = [
         "192.168.1.4",
